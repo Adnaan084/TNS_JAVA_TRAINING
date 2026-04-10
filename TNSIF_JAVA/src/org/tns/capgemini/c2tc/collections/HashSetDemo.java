be58b43dec320c92dd4fb51fc422d49068a84d49
Adnnan084 <adnaancareer@gmail.com>
2026-04-10 18:44:06 +0530

    used hash set on a demo

--- TNSIF_JAVA/src/org/tns/capgemini/c2tc/collections/HashSetDemo.java
+++ TNSIF_JAVA/src/org/tns/capgemini/c2tc/collections/HashSetDemo.java
@@ -1,9 +1,20 @@
 package org.tns.capgemini.c2tc.collections;
 
+import java.util.HashSet;
+
 public class HashSetDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashSet<String> set=new HashSet<>();
+		set.add("Apple");
+		set.add("Banana");
+		set.add("Cherry");
+		set.add(null);
+		set.add(null);
+		set.add("Apple");
+		
+		System.out.println("Hashset  " + set);
+		
 
 	}
 
