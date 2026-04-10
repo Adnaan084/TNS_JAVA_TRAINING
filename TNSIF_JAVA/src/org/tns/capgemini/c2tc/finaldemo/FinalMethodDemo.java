aa6e92782880143097b659ddd6fa9a4b42646278
Adnnan084 <adnaancareer@gmail.com>
2026-04-10 19:19:23 +0530

    error cleared

--- TNSIF_JAVA/src/org/tns/capgemini/c2tc/finaldemo/FinalMethodDemo.java
+++ TNSIF_JAVA/src/org/tns/capgemini/c2tc/finaldemo/FinalMethodDemo.java
@@ -2,33 +2,34 @@ package org.tns.capgemini.c2tc.finaldemo;
 
 class calc
 {
-	public final void show()
-	{
-		//code implementation
-		System.out.println("AI integration done by hema");
-	}
-	public void add(int a ,int b)
-	{
-		System.out.println(a+b);
-	}
+    public final void show()
+    {
+        System.out.println("AI integration done by hema");
+    }
+
+    public void add(int a, int b)
+    {
+        System.out.println(a + b);
+    }
 }
+
 class Advcalc extends calc
 {
-	public void show()
-	{
-		
-	}
-	public void add(int a ,int b)
-	{
-		System.out.println("welcome");
-	
-	}
+    // show() NOT allowed here
+
+    public void add(int a, int b)
+    {
+        System.out.println("welcome");
+    }
 }
-public class FinalMethodDemo {
 
-	public static void main(String[] args) {
-		
+public class FinalMethodDemo
+{
+    public static void main(String[] args)
+    {
+        Advcalc obj = new Advcalc();
 
-	}
-
+        obj.show();   // calls parent method
+        obj.add(10, 20);  // calls overridden method
+    }
 }
