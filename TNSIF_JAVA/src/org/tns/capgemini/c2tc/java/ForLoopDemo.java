44d5583ceef41065e98af626b83cc01d1d7927a1
Adnnan084 <adnaancareer@gmail.com>
2026-04-11 11:56:17 +0530

    error cleared

--- TNSIF_JAVA/src/org/tns/capgemini/c2tc/java/ForLoopDemo.java
+++ TNSIF_JAVA/src/org/tns/capgemini/c2tc/java/ForLoopDemo.java
@@ -3,7 +3,10 @@ package org.tns.capgemini.c2tc.java;
 public class ForLoopDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
 
 	}
 
