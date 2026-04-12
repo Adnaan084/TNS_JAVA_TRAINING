84bf8a1f6ed8dac1013b21cd7edd4a2dbc9827f4
Adnnan084 <adnaancareer@gmail.com>
2026-04-12 18:01:49 +0530

    with lambda expression

--- TNSIF_JAVA/src/org/tns/capgemini/c2tc/lambdaexpression/withLambdaExp.java
+++ TNSIF_JAVA/src/org/tns/capgemini/c2tc/lambdaexpression/withLambdaExp.java
@@ -1,10 +1,16 @@
 package org.tns.capgemini.c2tc.lambdaexpression;
 
-public class withLambdaExp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+@FunctionalInterface    
+interface Drawable{  
+    public void draw();  
+}  
+public class withLambdaExp {  
+    public static void main(String[] args) {  
+        int width=10;    
+        //with lambda  
+        Drawable d2=()->{  
+            System.out.println("Drawing "+width);  
+        };  
+        d2.draw();  
+    }  
+}  
