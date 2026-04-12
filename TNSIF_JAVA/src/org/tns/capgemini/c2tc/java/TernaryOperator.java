a88faad8f94500abb053305a719a60d9d14e4143
Adnnan084 <adnaancareer@gmail.com>
2026-04-12 13:32:20 +0530

    error cleared

--- TNSIF_JAVA/src/org/tns/capgemini/c2tc/java/TernaryOperator.java
+++ TNSIF_JAVA/src/org/tns/capgemini/c2tc/java/TernaryOperator.java
@@ -1,10 +1,11 @@
 package org.tns.capgemini.c2tc.java;
 
 public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        // Ternary operation
+        int max = (a > b) ? a : b;
+        System.out.println("Max value is: " + max);
+    }
 }
